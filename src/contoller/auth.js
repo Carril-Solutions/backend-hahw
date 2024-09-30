d478179f1426a6a56e81e643726ca61b7e623f09
navdeep_Sharmaa <navdeep.sharma@fictivebox.com>
2024-09-30 13:24:17 +0530

    timstamps added for iot dynamic data

--- src/contoller/auth.js
+++ src/contoller/auth.js
@@ -145,8 +145,13 @@ function generateToken() {
 exports.forgotPassword = async (req, res) => {
   try {
     const { email } = req.body;
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).send({ error: "User not found" });
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await Admin.findOne({ email });
+    }
+    if (!user) {
+      return res.status(400).send({ error: "User not found" });
+    }
 
     // Generate and store reset token
     const resetToken = generateToken();
