e3bea9ed4026f747194350f077d0d5c871e29ea8
pfictive <pooja@fictivebox.com>
2024-09-30 12:33:45 +0530

    added permission in route

--- src/routers/device.js
+++ src/routers/device.js
@@ -3,12 +3,11 @@ const router = express.Router();
 
 const ctrl = require("../contoller/device");
 const {verifyJwtToken} = require("../middlewares/auth");
-const { checkPermission } = require("../middlewares/checkPermission");
 
-router.post("/addDevice", verifyJwtToken, checkPermission("Add device"), ctrl.createDevice);
-router.put("/updateDevice/:deviceId", verifyJwtToken, checkPermission("Update device"), verifyJwtToken, ctrl.updateDevice);
-router.get("/devices", verifyJwtToken, checkPermission("View devices"), ctrl.getDevice);
-router.patch("/updateDeviceStatus/:deviceId", verifyJwtToken, checkPermission("Update device status"), ctrl.updateDeviceStatus);
-router.delete("/deleteDevice/:deviceId", verifyJwtToken, checkPermission("Delete device"), ctrl.deleteDevice);
+router.post("/addDevice", verifyJwtToken, ctrl.createDevice);
+router.put("/updateDevice/:deviceId", verifyJwtToken, verifyJwtToken, ctrl.updateDevice);
+router.get("/devices", verifyJwtToken, ctrl.getDevice);
+router.patch("/updateDeviceStatus/:deviceId", verifyJwtToken, ctrl.updateDeviceStatus);
+router.delete("/deleteDevice/:deviceId", verifyJwtToken, ctrl.deleteDevice);
 
 module.exports = router
--- src/routers/division.js
+++ src/routers/division.js
@@ -3,13 +3,12 @@ const router = express.Router();
 
 const ctrl = require("../contoller/division");
 const {verifyJwtToken, verifyOptionalJwtToken} = require("../middlewares/auth");
-const { checkPermission } = require("../middlewares/checkPermission");
 
-router.post("/addDivision", verifyJwtToken, checkPermission("Add division"), ctrl.createDivision);
-router.get("/divisions", verifyJwtToken, checkPermission("View divisions"), ctrl.getDivision);
-router.put("/updateDivision/:divisionId", checkPermission("Update division"), verifyJwtToken, ctrl.updateDivision);
-router.patch("/updateDivisionStatus/:divisionId", verifyJwtToken, checkPermission("Update division status"), ctrl.updateDivisionStatus);
-router.delete("/deleteDivision/:divisionId", verifyJwtToken, checkPermission("Delete division"), ctrl.deleteDivision);
+router.post("/addDivision", verifyJwtToken, ctrl.createDivision);
+router.get("/divisions", verifyJwtToken, ctrl.getDivision);
+router.put("/updateDivision/:divisionId", verifyJwtToken, ctrl.updateDivision);
+router.patch("/updateDivisionStatus/:divisionId", verifyJwtToken, ctrl.updateDivisionStatus);
+router.delete("/deleteDivision/:divisionId", verifyJwtToken, ctrl.deleteDivision);
 
 
 
