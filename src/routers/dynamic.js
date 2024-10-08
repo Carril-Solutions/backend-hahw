0fdba7039b16fc083292d84c3c4dec82fa825e03
pfictive <pooja@fictivebox.com>
2024-10-08 18:46:30 +0530

    fix: route

--- src/routers/dynamic.js
+++ src/routers/dynamic.js
@@ -4,6 +4,6 @@ const router = express.Router();
 const ctrl = require("../contoller/dynamic");
 
 router.post("/iot-data", ctrl.createDynamicModelandAddedData);
-router.get("/get-iot-transfer-data/:deviceId", ctrl.getIotData);
+router.get("/get-iot-data/:deviceId", ctrl.getIotData);
 
 module.exports = router
