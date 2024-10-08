017832b116e3242254cf1d466d0e993854914861
pfictive <pooja@fictivebox.com>
2024-10-08 18:28:54 +0530

    feat: Device tickets raised

---
+++ src/routers/deviceTicketRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const ctrl = require("../contoller/deviceTicketController");
+const { verifyJwtToken } = require("../middlewares/auth");
+
+router.post("/addDeviceTicket", verifyJwtToken, ctrl.createDeviceTicket);
+router.get("/deviceTickets", verifyJwtToken, ctrl.getAllDeviceTickets);
+router.put("/updateDeviceTicket/:ticketId", verifyJwtToken, ctrl.updateDeviceTicket);
+router.patch("/updateDeviceTicketStatus/:ticketId", verifyJwtToken, ctrl.updateDeviceTicketStatus);
+router.delete("/deleteDeviceTicket/:ticketId", verifyJwtToken, ctrl.deleteDeviceTicket);
+
+module.exports = router;
